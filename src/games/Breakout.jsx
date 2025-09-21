a67172d3ba7aab2153878e807108a8ccd4664b52
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-21 17:59:10 +0100

    draw function

--- src/games/Breakout.jsx
+++ src/games/Breakout.jsx
@@ -94,7 +94,29 @@ export default function Breakout() {
       }
     }
 
+    function draw() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      drawBricks()
+      drawBall()
+      drawPaddle()
+      collisionDetection()
 
+      if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) dx = -dx
+      if (y + dy < ballRadius) dy = -dy
+      else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) dy = -dy
+        else {
+          document.location.reload()
+        }
+      }
+
+      if (rightPressed && paddleX < canvas.width - paddleWidth) paddleX += 7
+      else if (leftPressed && paddleX > 0) paddleX -= 7
+
+      x += dx
+      y += dy
+      requestAnimationFrame(draw)
+    }
 
     draw()
   }, [])
