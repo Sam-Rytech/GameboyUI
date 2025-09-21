263afc4c44534ed26abb166c228d967562f1dfe4
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-21 17:58:13 +0100

    Draw ball function

--- src/games/Breakout.jsx
+++ src/games/Breakout.jsx
@@ -60,7 +60,13 @@ export default function Breakout() {
       }
     }
 
-
+    function drawBall() {
+      ctx.beginPath()
+      ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
+      ctx.fillStyle = '#0095DD'
+      ctx.fill()
+      ctx.closePath()
+    }
 
 
 
